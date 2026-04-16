067e48aced15788e49d0f309d8c0e97b5baa8643
tungsahur1 <n0l.sigma67@gmail.com>
2026-04-15 22:22:58 -0400

    Update dash.js
    balright

--- dash.js
+++ dash.js
@@ -1,57 +1,59 @@
-window.startDash = startDash;
 function startDash(){
 showScreen(null);
-game.style.display = "block";
+game.style.display="block";
 
-let x = 200;
-let y = game.height-150;
-let vel = 0;
+let x=200;
+let y=game.height-150;
+let vel=0;
 
-let spikes = [400,600,800];
+let spikes=[400,550,700,900];
 
-loop = setInterval(()=>{
+loop=setInterval(()=>{
 ctx.clearRect(0,0,game.width,game.height);
 
-// bg
-ctx.fillStyle="#111";
+// background (GD style)
+ctx.fillStyle="#0a0a1a";
 ctx.fillRect(0,0,game.width,game.height);
 
 // ground
-ctx.fillStyle="#333";
+ctx.fillStyle="#222";
 ctx.fillRect(0,game.height-100,game.width,100);
 
 // spikes
-ctx.fillStyle="red";
+ctx.fillStyle="#ff0044";
 spikes.forEach(s=>{
 ctx.beginPath();
 ctx.moveTo(s,game.height-100);
-ctx.lineTo(s+20,game.height-140);
+ctx.lineTo(s+20,game.height-150);
 ctx.lineTo(s+40,game.height-100);
 ctx.fill();
 
-if(x+40>s && x<s+40 && y+40>game.height-120){
+// FIXED collision
+if(x+40 > s && x < s+40 && y+40 > game.height-120){
 showScreen("menu");
 }
 });
 
 // cube
-ctx.fillStyle="cyan";
+ctx.fillStyle="#00ffff";
 ctx.fillRect(x,y,40,40);
 
 // jump
 if(keys[" "] && y>=game.height-150){
-vel = -15;
+vel=-18;
 }
 
-vel += 1;
-y += vel;
+vel+=1;
+y+=vel;
 
-if(y > game.height-150){
-y = game.height-150;
-vel = 0;
+if(y>game.height-150){
+y=game.height-150;
+vel=0;
 }
 
-x += 6;
+x+=7;
 
 },30);
 }
+
+window.startDash = startDash;
