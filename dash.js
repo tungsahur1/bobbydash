01a809a3b81beb163675b78a72317d6fdf4bf48a
tungsahur1 <n0l.sigma67@gmail.com>
2026-04-16 19:51:07 -0400

    dash.js
    gd?

--- dash.js
+++ dash.js
@@ -1,38 +1,50 @@
+let cube, spikes, progress=0;
+
 function startDash(){
 document.querySelectorAll(".screen").forEach(s=>s.classList.remove("active"));
 canvas.style.display="block";
 
 running=true;
-loopType="dash";
+mode="dash";
 
 cube={x:150,y:300,vy:0};
 spikes=[{x:600},{x:900}];
+progress=0;
 
-requestAnimationFrame(gameLoop);
+requestAnimationFrame(loop);
 }
-
-let cube, spikes;
 
 function dashGame(){
 
-ctx.fillStyle="#001122";
+/* BACKGROUND CHANGE */
+let p = progress/100;
+if(p<0.35) ctx.fillStyle="red";
+else if(p<0.7) ctx.fillStyle="purple";
+else ctx.fillStyle="blue";
+
 ctx.fillRect(0,0,canvas.width,canvas.height);
 
+/* PROGRESS */
+progress+=0.1;
+
+/* GRAVITY */
 cube.vy+=0.5;
 cube.y+=cube.vy;
 
-if(keys[" "] && cube.y>=300){
+/* JUMP */
+if((keys[" "]||keys["w"]||keys["arrowup"]) && cube.y>=300){
 cube.vy=-8;
 }
 
+/* FLOOR */
 if(cube.y>300){
 cube.y=300;
 cube.vy=0;
 }
 
+/* SPIKES */
 spikes.forEach(s=>{
 s.x-=4;
-
 if(s.x<-20) s.x=canvas.width+200;
 
 if(cube.x<s.x+20 && cube.x+20>s.x && cube.y+20>300){
@@ -40,10 +52,15 @@ stopGame();
 show("menu");
 }
 
-ctx.fillStyle="red";
+ctx.fillStyle="black";
 ctx.fillRect(s.x,300,20,20);
 });
 
+/* DRAW */
 ctx.fillStyle="cyan";
 ctx.fillRect(cube.x,cube.y,20,20);
+
+/* BAR */
+ctx.fillStyle="white";
+ctx.fillRect(0,10,progress*5,10);
 }
